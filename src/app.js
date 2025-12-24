fcfbe023ee22c43d5ad81cdafa0280bd6221a3c3
Ritesh Gite <riteshgite2005@gmail.com>
2025-12-24 17:09:05 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -48,6 +48,49 @@ app.use("/test", (req, res) => {
     res.send("test test test");
 })
 
+app.get(/^\/ab?c$/, (req, res) => {
+    res.send("you can write, abc or ac");
+})
+
+app.get(/^\/ab+c$/, (req, res) => {
+    res.send("write b one time or multiple time")
+})
+
+app.get(/^\/ab(cde)?f$/, (req, res) => {
+    res.send("cde is optional")
+})
+
+app.get(/^\/ab(cde)*f$/, (req, res) => {
+    res.send("write cde 0 times or multiple times");
+})
+
+app.get(/^\/ab.*c$/, (req, res) => {
+    console.log(req.url);
+    res.send("You can add anything in between b and c", req.url);
+})
+
+app.get("/profile/:userId/:connectionId", (req, res) => {
+    console.log(req.params);
+    res.send(req.params);
+})
+
+app.get("/profile", (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.get(/ab(cde)+f/, (req, res) => {
+    res.send("write cde 1 time or multiple times");
+})
+
+app.get(/ab*c/, (req, res) => {
+    res.send("regex pattern");
+})
+
+app.get(/ab.c/, (req, res) => {
+    res.send("write any one character in between b and c");
+})
+
 app.listen(1023, (req, res) => {
     console.log("server listening");
 })
