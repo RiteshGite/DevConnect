9726b7d3cc9a7d09fc49b20bde47fc6455c23200
Ritesh Gite <riteshgite2005@gmail.com>
2026-01-02 09:46:10 +0530

    get API'S

--- src/app.js
+++ src/app.js
@@ -5,18 +5,47 @@ const app = express();
 
 const { connectDb } = require("./config/db");
 
+app.use(express.json());
+
+// post the user data 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kohli",
-        email: "ViratKohli@gmail.com",
-        password: "virat100"
-    })
+
+    const user = new User(req.body);
+
     try {
-        await user.save()
+        await user.save();
         res.send("user added successfully");
     } catch(err) {
         res.status(400).send("Error saving the user : " + err);
+    }
+})
+
+//  get the user by email id
+app.get("/user", async (req, res) => {
+    const { emailId } = req.body;
+    try {
+        const user = await User.findOne({emailId: emailId});
+        if (!user) {
+            res.send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+// Feed API - get all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if(!users.length) {
+            res.send("No users found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
     }
 })
 
