250b59266961d9f111afc1c71b898fe17542c87f
Ritesh Gite <riteshgite2005@gmail.com>
2026-01-28 07:23:53 +0530

    fix env and db issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-    const uri = process.env.MONGO_URI;
-    console.log("uri = ", uri);
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to DB");
     } catch (err) {
         console.error("DB connection failed:", err);
