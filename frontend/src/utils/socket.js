c97ace8fa38b369869f3dc167e7502e303939f68
Ritesh Gite <riteshgite2005@gmail.com>
2026-02-11 14:22:04 +0530

    fix

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,16 +1,9 @@
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-    
-    if(location.hostname === "localhost") {
-        return io(BASE_URL, {
-            withCredentials: true,
-        });
-    } else {
-        return io("/", {
-            path: "/api/socket.io",
-            withCredentials: true
-        });
-    }
+    return io(BASE_URL, {
+        withCredentials: true,
+        transports: ["websocket"],
+    });
 };
